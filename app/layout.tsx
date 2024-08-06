01482dfd363fd2e7d257c2469d534045e8b989ee
이태성 <ga1754@naver.com>
2024-08-06 22:22:48 +0900

    [init] 포트폴리오 만들기 프로젝트 시작

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+// components
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
+
+const jetbrains_Mono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
+});
+
+export const metadata: Metadata = {
+  title: "LEE TAE SEONG",
+  description: "my portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={jetbrains_Mono.variable}>
+        <StairTransition />
+        <Header />
+        <PageTransition>{children}</PageTransition>
+      </body>
+    </html>
+  );
+}
---
+++ components/Stairs.tsx
@@ -0,0 +1,51 @@
+import { animate, motion } from "framer-motion";
+
+// variations
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+// calculate the reverse index for staggered delay
+const reverseIndex = (index: number) => {
+  const totalSteps = 6; // number of steps
+  return totalSteps - index - 1;
+};
+
+{
+  /* render & motion divs, each representing a step of the stairs. 
+    Each div will have the same animation defined by the stairsAnimation object.
+    The delay for each div is calculated dynamically based on It's reversed index,
+    creating a staggered effect with decreasing delay for each subsequent step.
+    */
+}
+export default function Stairs() {
+  return (
+    <>
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
+}
